16168b95143cdb53365c6d59e5002a0e09c1c633
jaysun001 <jaysunu58@gmail.com>
2025-05-08 14:34:27 +0530

    aws update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT ;
 app.use(cors({
   // origin: 'http://localhost:3000', // or your frontend URL
   // origin: 'https://the-paxful.netlify.app',
-  origin: 'https://trading-client.onrender.com',
+  origin: 'https://3.6.126.103',
   credentials: true,
 })); // CORS setup
 app.use(express.json()); // Parse JSON bodies
